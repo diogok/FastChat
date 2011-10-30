20be4ce5c9b2dfcc1821ba0ff6ba696a5c4b0a9d
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2011-10-29 23:55:16 -0200

    added redis

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :description "Websocket fastchat"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"] 
-                 [org.webbitserver/webbit "0.2.1"]]
+                 [org.webbitserver/webbit "0.2.1"]
+                 [clj-redis "0.0.12"]]
   :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]] 
   :repositories {"stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
   :jvm-opts ["-Xmx512m" "-server" "-XX:+UseConcMarkSweepGC"])
-
