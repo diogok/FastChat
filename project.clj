a206548228fcf715e301d8cddcd7feda39ac108c
Diogo Souza da Silva <manifesto@manifesto.blog.br>
2012-01-09 14:36:37 -0200

    webbit websocket version version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject fastchat "0.1"
+(defproject fastchat "0.2"
   :main fastchat.server
   :description "Websocket fastchat"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"] 
-                 [org.webbitserver/webbit "0.2.1"]
+                 [org.webbitserver/webbit "0.3.0"]
                  [clj-redis "0.0.12"]]
   :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]] 
   :repositories {"stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
